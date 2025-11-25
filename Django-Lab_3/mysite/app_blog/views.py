c940c529a6b6dfdbea99ea7261e936507a45f75d
ihor_hryhorash211 <125390896+white13crow@users.noreply.github.com>
2025-11-25 12:15:45 +0200

    Add files via upload

---
+++ Django-Lab_3/mysite/app_blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
